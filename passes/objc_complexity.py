05ba3cd3ada649229421855a179118e4848ffefa
Dmitry Ivanov <ethercrow@gmail.com>
2012-01-21 17:27:47 +0700

    tune complexity metric

--- passes/objc_complexity.py
+++ passes/objc_complexity.py
@@ -4,14 +4,6 @@ from diagnostic import LintDiagnostic
 from passes.base.token_pass_base import TokenPassBase
 from utils import full_text_for_cursor
 
-# blocks and branching points
-COMPLEX_KINDS = (ci.CursorKind.IF_STMT,
-                 ci.CursorKind.WHILE_STMT,
-                 ci.CursorKind.FOR_STMT,
-                 ci.CursorKind.SWITCH_STMT,
-                 ci.CursorKind.BLOCK_EXPR)
-
-
 class ObjCMethodComplexity(TokenPassBase):
 
     def __init__(self):
@@ -23,10 +15,13 @@ class ObjCMethodComplexity(TokenPassBase):
 
     def maybe_diagnostic(self, cur):
 
-        def calculate_complexity(c, depth=1):
+        # depth -1 -- OBJC_INSTANCE_METHOD_DECL (method signature)
+        # depth  0 -- COMPOUND_STATMENT (curly braces)
+        # depth  1 -- actual method code
+        def calculate_complexity(c, depth=-1):
             result = 0
-            if c.kind in COMPLEX_KINDS:
-                result = depth
+            if c.kind == ci.CursorKind.COMPOUND_STMT and depth>0:
+                result = 1 if (depth < 3) else 2
 
             result += sum(calculate_complexity(child, depth + 1)
                           for child in c.get_children())
@@ -37,8 +32,8 @@ class ObjCMethodComplexity(TokenPassBase):
         line_count = full_text_for_cursor(cur).count('\n')
 
         # TODO configurable thresholds
-        if complexity > 30 or\
-           complexity > 5 and line_count > 50:
+        if complexity > 15 or\
+           complexity > 3 and line_count > 50:
 
             d = LintDiagnostic()
             d.line_number = cur.location.line
