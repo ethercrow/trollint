6af4f6ef960c6a3ef3e942bc164accac176b38b2
Dmitry Ivanov <divanov@noveogroup.com>
2012-01-27 18:41:45 +0700

    inspect classmethod declarations

--- passes/objc_method_decls.py
+++ passes/objc_method_decls.py
@@ -6,6 +6,7 @@ import re
 from utils import full_text_for_cursor
 
 re_method_decl = re.compile(r'^- \([^\)]+\)\w.*$')
+re_class_method_decl = re.compile(r'^\+ \([^\)]+\)\w.*$')
 
 
 class ObjCMethodDecls(TokenPassBase):
@@ -16,16 +17,21 @@ class ObjCMethodDecls(TokenPassBase):
 
         self.cursor_kind = cindex.CursorKind.OBJC_INSTANCE_METHOD_DECL
         self.category = 'Style'
+        self.message = ("instance method declaration whitespace doesn't match "
+                        "template '- (Foo)barBaz:(Baz)baz'")
+        self.regex = re_method_decl
+        self.first_char = '-'
+
 
     def maybe_diagnostic(self, cur):
         first_line = full_text_for_cursor(cur).split('\n')[0]
 
-        if first_line[0] == '-' and not re_method_decl.match(first_line):
+        if first_line[0] == self.first_char and\
+                not self.regex.match(first_line):
 
             d = LintDiagnostic()
             d.line_number = cur.location.line
-            d.message = "method declaration whitespace doesn't match "\
-                        "template '- (Foo)barBaz:(Baz)baz'"
+            d.message = self.message
             d.filename = cur.location.file.name
             d.context = first_line
             d.category = self.category
@@ -33,3 +39,16 @@ class ObjCMethodDecls(TokenPassBase):
             return d
 
         return None
+
+class ObjCClassMethodDecls(ObjCMethodDecls):
+
+    def __init__(self):
+
+        super(ObjCClassMethodDecls, self).__init__()
+
+        self.cursor_kind = cindex.CursorKind.OBJC_CLASS_METHOD_DECL
+        self.message = ("class method declaration whitespace doesn't match "
+                        "template '+ (Foo)barBaz:(Baz)baz'")
+        self.regex = re_class_method_decl
+        self.first_char = '+'
+
